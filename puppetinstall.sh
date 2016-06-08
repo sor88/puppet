263a8605c1550aed279e02e9ee9284024006a22d
sor88 <plus@omsklug.com>
2016-06-08 17:17:17 +0500

    Переработана проверка версий ОС. Добавлена поддержка Debian 7,8. Исправлен сценарий Debian

--- puppetinstall.sh
+++ puppetinstall.sh
@@ -24,7 +24,9 @@ installp(){
                                 sleep 2
                                 sed -i s/START=no/START=yes/g /etc/default/puppet
                                 sed -i "/\/var\/log\/puppet/a \server=$namepuppet" /etc/puppet/puppet.conf
-                                puppet agent --test
+                                sleep 10
+				puppet agent --enable
+				puppet agent --test
                                 ) | whiptail --title "Установка Puppet клиента" --gauge "Пожалуйста подождите, роняем цены на нефть" 7 70 1
                                 rm .log.log
                                 exit
@@ -33,23 +35,23 @@ installp2(){
 (
                                 wget -q apt.puppetlabs.com/puppetlabs-release-$version.deb
                                 echo 5
-                                dpkg -i puppetlabs-release-pc1-$version.deb > /dev/null
+                                dpkg -i puppetlabs-release-$version.deb > /dev/null
                                 echo 10
-                                rm puppetlabs-release-pc1-$version.deb
+                                rm puppetlabs-release-$version.deb
                                 echo 15
                                 apt-get install -y puppet > .log.log
                                 f1
                                 sleep 2
-                                sed -i s/START=no/START=yes/g /etc/default/puppet
                                 sed -i "/\/var\/log\/puppet/a \server=$namepuppet" /etc/puppet/puppet.conf
-                                puppet agent --test
+				sleep 10
+				puppet agent --enable
+				puppet agent --test
                                 ) | whiptail --title "Установка Puppet клиента" --gauge "Пожалуйста подождите, роняем цены на нефть" 7 70 1
                                 rm .log.log
                                 exit
 }
 if (whiptail --title "Подготовка рабочей станции " --yesno "Запуск сценария установки" 10 60  "введите имя ПК") then
-#		namehost=$(whiptail --title "Установка HostName" --inputbox "Введите имя компьютера" 10 60 HostName 3>&1 1>&2 2>&3)
-		namehost=$(whiptail --title "Установка HostName" --inputbox "Введите имя компьютера" 10 60 itdeb 3>&1 1>&2 2>&3)
+		namehost=$(whiptail --title "Установка HostName" --inputbox "Введите имя компьютера" 10 60 HostName 3>&1 1>&2 2>&3)
 		exitstatus=$?
 			if [ $exitstatus = 0 ]; then
 				echo $namehost > /etc/hostname
@@ -57,8 +59,7 @@ if (whiptail --title "Подготовка рабочей станции " --yes
 				echo "Отмена"
 				exit
 			fi
-#namepuppet=$(whiptail --title "Введите адрес сервера Puppet" --inputbox "Введите имя сервера Puppet" 10 60 ServerName 3>&1 1>&2 2>&3)
-namepuppet=$(whiptail --title "Введите адрес сервера Puppet" --inputbox "Введите имя сервера Puppet" 10 60 puppet.edushd.ru 3>&1 1>&2 2>&3)
+namepuppet=$(whiptail --title "Введите адрес сервера Puppet" --inputbox "Введите имя сервера Puppet" 10 60 ServerName 3>&1 1>&2 2>&3)
 exitstatus=$?
 			if [ $exitstatus = 0 ]; then
 testversion=$(lsb_release -r | sed 's/.\+:\s\+//');
