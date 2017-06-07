a98fb386446627233c93cba83fac5ab613dcd0ae
Plus <plus@omsklug.com>
2017-06-07 15:18:15 +0500

    Update init.pp

--- zabbix-client/manifests/init.pp
+++ zabbix-client/manifests/init.pp
@@ -8,7 +8,7 @@ class zabbix-client {
 class zabbix-client::zabconf {
 file { '/etc/zabbix/zabbix_agentd.conf':
   ensure  => file,
-  content => template("$module_name//zabbix.conf.erb"),
+  content => template("$module_name/zabbix.conf.erb"),
 }
 
 }
